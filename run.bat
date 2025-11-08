773f88b0490b094d5d36e033b0c1ff2739b32580
TheUsernameIsNotLong <hmike3030@gmail.com>
2025-11-08 00:43:16 +0000

    automatic setup upon missing venv

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
 @echo off
+
+SET VENV_DIR=venv
+
+IF NOT EXIST %VENV_DIR% (
+    ECHO Creating virtual environment...
+    call setup.bat
+)
+
 venv\Scripts\python.exe src\main.py
 pause
--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+VENV_DIR="venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    ./setup.sh
+fi
+
 .venv/bin/python src/main.py
